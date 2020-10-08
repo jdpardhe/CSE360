f70277086501a356644a824c9fdd2faf94fec68e
Joshua Pardhe <64713772+jdpardhe@users.noreply.github.com>
2020-10-07 22:05:10 -0700

    Initial Check-in

---
+++ CSE360Assignment2/src/cse360assignment02/AddingMachine.java
@@ -0,0 +1,34 @@
+/**
+* Joshua Pardhe 1214553301
+* CSE360 - Professor Gonzales-Sanchez
+* This is the first version of the program, copied from the assignment page.
+* This program has empty functions, but eventually will function as a basic calculator.
+*/
+
+
+package cse360assignment02;
+
+public class AddingMachine {
+  private int total;
+  
+  public AddingMachine () {
+    total = 0;  // not needed - included for clarity
+  }
+  
+  public int getTotal () {
+    return 0;
+  }
+  
+  public void add (int value) {
+  }
+
+  public void subtract (int value) {
+  }
+
+  public String toString () {
+    return "";
+  }
+
+  public void clear() {
+  }
+}
