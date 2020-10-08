967dd1b92365f9d1577b18c0937303573cd75581
Joshua Pardhe <64713772+jdpardhe@users.noreply.github.com>
2020-10-07 22:25:37 -0700

    Version 2!
    Completed the program as per the assignment requirements.

--- CSE360Assignment2/src/cse360assignment02/AddingMachine.java
+++ CSE360Assignment2/src/cse360assignment02/AddingMachine.java
@@ -1,8 +1,10 @@
 /**
 * Joshua Pardhe 1214553301
 * CSE360 - Professor Gonzales-Sanchez
-* This is the first version of the program, copied from the assignment page.
-* This program has empty functions, but eventually will function as a basic calculator.
+* This is the second version of the program with updated functions and variables.
+* This program can add and subtract, and return the current value.
+* It can also return a ticker tape of total calculations performed.
+* Finally, you can call the clear() function to clear the current sum and the tape.
 */
 
 
@@ -10,25 +12,45 @@ package cse360assignment02;
 
 public class AddingMachine {
   private int total;
+  private String calcHistory = "0";
   
   public AddingMachine () {
     total = 0;  // not needed - included for clarity
   }
   
   public int getTotal () {
-    return 0;
+    return total;
   }
   
   public void add (int value) {
+	  total = total + value;
+	  calcHistory += " + " + value;
   }
 
   public void subtract (int value) {
+	  total = total - value;
+	  calcHistory += " - " + value;
   }
 
   public String toString () {
-    return "";
+    return calcHistory;
   }
 
   public void clear() {
+	  total = 0;
+	  calcHistory = "0";
   }
+  
+//  public static void main(String[] args) {
+//	  AddingMachine myCalculator = new AddingMachine();
+//	  myCalculator.add (4); 
+//	  myCalculator.subtract (2); 
+//	  myCalculator.add(5);
+//	  System.out.println(myCalculator.getTotal());
+//	  System.out.println(myCalculator.toString());
+//	  myCalculator.clear();
+//	  System.out.println(myCalculator.getTotal());
+//	  System.out.println(myCalculator.toString());
+//  }
+  
 }
